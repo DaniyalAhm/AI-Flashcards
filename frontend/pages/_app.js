483888f7fc675b31df101a85ccd7821da7469e7c
Faith Nchang <143855102+Faith-Nchang@users.noreply.github.com>
2024-08-17 21:41:33 -0400

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,13 @@
 // pages/_app.js
 import '/home/daniyal/FlashCard-App/frontend/src/app/globals.css'
 
+import { ClerkProvider } from '@clerk/nextjs';
+
+
 function MyApp({ Component, pageProps }) {
+  <ClerkProvider>
   return <Component {...pageProps} />
+  </ClerkProvider>
 }
 
 export default MyApp;
